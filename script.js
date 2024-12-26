7018c0b6d36abecce86be9a2bdce5ee824754eb4
alaminkhan786 <freelanceralamin10219@gmail.com>
2024-12-26 21:16:08 +0600

    break

--- script.js
+++ script.js
@@ -1,13 +1,52 @@
+document.getElementById('firstUnit').value = 1;
+
 const plusBtn = document.getElementById('plusBtn');
+const productPrice = document.getElementById('productPrice');
+const totalAmount = document.getElementById('totalAmount');
+
 
 plusBtn.addEventListener('click', function(){
-    const firstInput = document.getElementById('firstInput');
-    const firstInputValue = parseFloat(firstInput.value);
-    firstInput.value = firstInputValue + 1;
-
-    const productPrice = document.getElementById('productPrice');
-    const currentProductPrice = parseFloat(productPrice.innerText);
-    productPrice.innerText = currentProductPrice * firstInput.value;
+    const firstUnit = document.getElementById('firstUnit');
+    const newfirstUnit = parseInt(firstUnit.value);
+    firstUnit.value = newfirstUnit + 1;
     
+    const productPriceValue = parseFloat(productPrice.innerText);
+    const newProductTotal = productPriceValue + 1219;
+    productPrice.innerText = newProductTotal;
+
+    const subTotal = document.getElementById('subTotal');
+    const netSubTotal = parseFloat(subTotal.innerText);
+    const lima = netSubTotal + 1219;
+    subTotal.innerText = lima;
+
+    const tax = parseFloat(document.getElementById('tax').innerText);
+    const newTotalAmount = tax + lima;
+    totalAmount.innerText = newTotalAmount;
+
+    console.log(netSubTotal);
 
 })
+
+const minusBtn = document.getElementById('minusBtn');
+
+minusBtn.addEventListener('click', function(){
+    const firstUnit = document.getElementById('firstUnit');
+    const newfirstUnit = parseInt(firstUnit.value);
+    firstUnit.value = newfirstUnit - 1;
+    
+    if (firstUnit.value <= 0) {
+        alert('Product amount cannot Zero');
+        firstUnit.value = 1;
+    }
+    else{
+        const productPriceValue = parseFloat(productPrice.innerText);
+        const newProductTotal = productPriceValue - 1219;
+        productPrice.innerText = newProductTotal;
+    
+        const subTotal = document.getElementById('subTotal');
+        const netSubTotal = parseFloat(subTotal.innerText);
+    
+        subTotal.innerText = netSubTotal - 1219;
+    }
+})
+
